c4434102f2108083e4ca1e1eed6dce9a55c1f253
JosephShortt <josephshortt1@gmail.com>
2024-12-09 21:58:00 +0000

    Added some styling using bootstrap

--- src/components/SearchByID.js
+++ src/components/SearchByID.js
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from 'react';
 import axios from "axios";
-
+import { Card } from 'react-bootstrap';
 
 function SearchByID() {
     const { id } = useParams(); // Extract the ID from the URL
@@ -20,18 +20,23 @@ function SearchByID() {
     }, [id]);
 
     if (!entry) {
-        return <div>No entries matching that ID...</div>; 
+        return <div>No entries matching that ID...</div>;
     }
 
     return (
         <div>
-            <h2>Entry Details</h2>
-            <p><strong>Steps:</strong> {entry.steps}</p>
-            <p><strong>Distance:</strong> {entry.distance}</p>
-            <p><strong>Weight:</strong> {entry.weight}</p>
-            <p><strong>Calories In:</strong> {entry.caloriesIn}</p>
-            <p><strong>Calories Out:</strong> {entry.caloriesOut}</p>
-            <p><strong>Date:</strong> {entry.date}</p>
+            <Card>
+                <Card.Header>Date: {entry.date}</Card.Header>
+                <Card.Body>
+                    <h2>Entry Details</h2>
+                    <p><strong>Steps:</strong> {entry.steps}</p>
+                    <p><strong>Distance:</strong> {entry.distance}</p>
+                    <p><strong>Weight:</strong> {entry.weight}</p>
+                    <p><strong>Calories In:</strong> {entry.caloriesIn}</p>
+                    <p><strong>Calories Out:</strong> {entry.caloriesOut}</p>
+                    <p><strong>Date:</strong> {entry.date}</p>
+                </Card.Body>
+            </Card>
         </div>
     );
 }
